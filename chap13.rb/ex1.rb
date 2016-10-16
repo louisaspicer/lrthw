95426d371566349d04d68c8ae0307b4fa13ca229
Louisa <louisaspicer@gmail.com>
2016-10-16 20:38:41 +0100

    started using ARGV

---
+++ chap13.rb/ex1.rb
@@ -0,0 +1,7 @@
+#ARGV is the "argument variable". This variable holds the argument
+#you pass to your Ruby script when you run it.
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
