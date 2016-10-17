16218684909e81a2b22dd457fe8c8feccc666158
Louisa <louisaspicer@gmail.com>
2016-10-17 14:23:55 +0100

    Changed script to use gets.chomp only

--- chap15/ex1.rb
+++ chap15/ex1.rb
@@ -1,5 +1,6 @@
-#get's filename from the argument variable stated when script is run
-filename = ARGV.first
+print "please enter filename you would like to read: "
+
+filename = gets.chomp
 
 #a new variable is created, which is the opened file 'filename'
 #open takes a parameter and returns a value which you can set to your own variable
