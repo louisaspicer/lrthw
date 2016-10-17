48d04778784df2470ef4bb71879903db5db1bfea
Louisa <louisaspicer@gmail.com>
2016-10-17 14:31:04 +0100

    closing file

--- chap15/ex1.rb
+++ chap15/ex1.rb
@@ -4,9 +4,16 @@ filename = gets.chomp
 
 #a new variable is created, which is the opened file 'filename'
 #open takes a parameter and returns a value which you can set to your own variable
+#this makes a "File object"
 txt = open(filename)
 
 #prints out what the filename from the ARGV is.
 puts "Here's your file #{filename}:"
 #prints out the txt files contents
+print txt.read
+
+#important to close files when you are done with them
+txt.close
+
+#checking file is actually closed
 print txt.read
