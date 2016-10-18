e42cc64baefe0a2531f1dd7d8d7c4cdb5fbeaa56
Louisa <louisaspicer@gmail.com>
2016-10-18 11:13:53 +0100

    creating functions checklist

---
+++ chap18/functions_checklist.rb
@@ -0,0 +1,12 @@
+puts "Function checklist:"
+
+puts """
+\t* Start your function with def.
+\t* Function name should only have characters and underscores.
+\t* Put an open parenthesis ( right after function name.
+\t* Put your arguments after the parenthesis, separated by commas.
+\t* Make each argument unique (no duplicates).
+\t* Put a close parenthesis ) after the arguments.
+\t* Indent all lines of code you want in the function by two spaces.
+\t* End your function with end lined up with def at the above.
+"""
