791aa742fb8b2bbfec9ddcab484b6f178e828a39
Louisa <louisaspicer@gmail.com>
2016-10-18 13:35:45 +0100

    using shorthand notation +=

--- chap20/ex1.rb
+++ chap20/ex1.rb
@@ -1,13 +1,13 @@
 input_file = ARGV.first
 
 #this prints out the text within the document by using the .read method
-# f is a File object
+# f will be a File object
 def print_all(f)
   puts f.read
 end
 
 #the .read method plays the file like a tape, so when it's done it leaves
-#it at the end. so the seek method below. rewinds the tape to the start
+#it at the end. so the seek method below rewinds the tape to the start
 #rewinds to 0 bytes.
 def rewind(f)
   f.seek(0)
@@ -15,7 +15,7 @@ end
 
 #f.gets.chomp, takes one line from the current file (which is the opened input file)
 #f.gets reads from the file, until it encounters a new line (\n) character
-#the new line is read, but nothing more
+#the newline is read, but nothing more
 #chomp chomps off the newline character, just printing the line without a new line
 #chomp returns the string with the trailing line removed.
 def print_a_line(line_count, f)
@@ -28,7 +28,7 @@ current_file = open(input_file)
 puts "First let's print the whole file:\n"
 
 #the (f)  in defining the method represents the 'current file' here
-#so what is done to f in the method is done to the current file
+#so what is done to f in the method creation is done to the current file
 print_all(current_file)
 
 puts "Now let's rewind, kind of like a tape."
@@ -38,12 +38,15 @@ rewind(current_file)
 puts "Let's print three lines:"
 
 current_line = 1
+
 #uses the current line variable which is 1, and the current file variable
 #which is the open input file. 'f.gets.chomp' is applied to the 'current file
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+#this line acts the same as current_line = current_line + 1
+#it adds and resets variable value
+current_line += 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
