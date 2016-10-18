019c1e9bc39361d68f43fbb5b57038733c469c79
Louisa <louisaspicer@gmail.com>
2016-10-18 10:44:26 +0100

    shortening file more

--- chap17/ex2.rb
+++ chap17/ex2.rb
@@ -1,9 +1,7 @@
 from_file, to_file = ARGV ; in_file = open(from_file) ; indata = in_file.read
 puts "Copying from #{from_file} (#{indata.length} bytes) to #{to_file} (Exist = #{File.exist?(to_file)})..."
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
+out_file = open(to_file, 'w') ; out_file.write(indata)
 puts "Copying complete"
 
-out_file.close
-in_file.close
+out_file.close ; in_file.close
