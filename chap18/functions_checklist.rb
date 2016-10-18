d307d570c9b305b3986985c99e7c04f4955c201a
Louisa <louisaspicer@gmail.com>
2016-10-18 11:16:59 +0100

    updating checklist

--- chap18/functions_checklist.rb
+++ chap18/functions_checklist.rb
@@ -1,3 +1,4 @@
+
 puts "Function checklist:"
 
 puts """
@@ -9,4 +10,16 @@ puts """
 \t* Put a close parenthesis ) after the arguments.
 \t* Indent all lines of code you want in the function by two spaces.
 \t* End your function with end lined up with def at the above.
+
+"""
+
+puts "Calling the function:"
+
+puts """
+\t* Call/use/run the function by typing it's name.
+\t* Put the ( character after the name to run it.
+\t* Put the values you want into the parenthesis separated by commas.
+\t* End the function call with a ) character.
+\t* Functions that don't have parameters do not need the ().
+\t* To 'run', 'call', or 'use' a function all mean the same thing.
 """
