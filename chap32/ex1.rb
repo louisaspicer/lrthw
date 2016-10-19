18302b65121652da496fd8d214880e0b16f17164
Louisa <louisaspicer@gmail.com>
2016-10-19 14:29:10 +0100

    changed traditional for-loop and changed push(i) to << i

--- chap32/ex1.rb
+++ chap32/ex1.rb
@@ -2,10 +2,7 @@ the_count = [1, 2, 3, 4, 5]
 fruits = ['apples', 'oranges', 'pears', 'apricots']
 change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 
-#goes through a list in a more traditional style found in other langs
-for number in the_count
-  puts "This is count #{number}"
-end
+the_count.each {|n| puts "This is count #{n}" }
 
 #same as above, but in a more Ruby style
 #for every fruit in the array, label it as 'fruit' and
@@ -22,11 +19,13 @@ change.each {|i| puts "I got #{i}" }
 #creates a new empty array to work with
 elements = []
 
-#range 0 to 5, including 5
+# (..) range 0 to 5, including 5
+# (...) range 0 to 4 (doesn't include 5)
 (0..5).each do |i|
   puts "adding #{i} to the list."
-  #pushes the i variable on the *end* of the list.
-  elements.push(i)
+  #.push(i) can be used to push the i variable on the *end* of the list.
+  # or << i can be used
+  elements << i
 end
 
 elements.each {|i| puts "Element was: #{i}" }
