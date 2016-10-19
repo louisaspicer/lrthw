939c0b75f75d23b0c106c148b88057565a4adb75
Louisa <louisaspicer@gmail.com>
2016-10-19 10:46:42 +0100

    added comments

--- chap25/ex1.rb
+++ chap25/ex1.rb
@@ -1,6 +1,10 @@
+#when we 'require' this file in irb, we can use the below methods in this module
+#to run things quicker.
+
 module Ex25
 
   # This function will break up words for us.
+  #this breaks the words up into an array, splitting whenever there is a space.
   def Ex25.break_words(stuff)
     words = stuff.split(' ')
     return words
@@ -10,6 +14,7 @@ module Ex25
   def Ex25.sort_words(words)
     return words.sort
   end
+
 
   # Prints the first word after shifting it off.
   def Ex25.print_first_word(words)
@@ -24,12 +29,19 @@ module Ex25
   end
 
   # Takes in a full sentence and returns the sorted words.
+  # uses a previously made method within this method, using the given argument in that method
+  # to return a value using another previously made method.
   def Ex25.sort_sentence(sentence)
     words = Ex25.break_words(sentence)
     return Ex25.sort_words(words)
   end
 
   # Prints the first and last words of the sentence.
+  #breaks down the sentence into words in an array
+  #and saves it in a variable called words
+  #runs the previously made print first word method with the words as the parameter
+  #and also runs the print last word method with the words as the parameter
+  #the sentence has to be split into an array otherwise its just one string. 
   def Ex25.print_first_and_last(sentence)
     words = Ex25.break_words(sentence)
     Ex25.print_first_word(words)
