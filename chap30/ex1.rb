e73a9faa87289af605d622392de8cbf0281d5b0e
Louisa <louisaspicer@gmail.com>
2016-10-19 13:15:59 +0100

    added notes

--- chap30/ex1.rb
+++ chap30/ex1.rb
@@ -1,8 +1,10 @@
-people = 30
-cars = 40
-trucks = 15
+people = 300
+cars = 404
+trucks = 152
 
-if cars > people
+#if number of cars is more than people OR cars are more than trucks#
+#then print the line below, if not carry on to the next line.
+if cars > people || cars > trucks
   puts "We should take the cars."
 elsif cars < people
   puts "We should not take the cars."
@@ -10,7 +12,9 @@ else
   puts "We can't decide."
 end
 
-if trucks > cars
+#if trucks are more than cars AND cars are more than people
+#then execute the line underneath.
+if trucks > cars && cars > people
   puts "That's too many trucks."
 elsif trucks < cars
   puts "Maybe we could take the trucks."
@@ -18,6 +22,9 @@ else
   puts "We still can't decide."
 end
 
+#if there is only two choices, then no need for elsif
+#if there is more than two choices, elsif should be used
+#the last one should always been else
 if people > trucks
   puts "Alright, let's just take the trucks."
 else
