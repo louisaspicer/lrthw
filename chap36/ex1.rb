299e54c4458af8d9c42caf746839947f6af42747
Louisa <louisaspicer@gmail.com>
2016-10-19 22:23:33 +0100

    rules for if-statements

---
+++ chap36/ex1.rb
@@ -0,0 +1,9 @@
+# Every if-statement must have an else
+# If this else should never run because it doesn't make sense then you must use
+# a die function in the else that prints out an error message and dies. This
+# will find many errors.
+# Never nest if-statements more than two deep, and always try to do them one deep
+# Treat if-statements like paragraphs. Where each if-elsif-else grouping is like
+# a set of sentences. Put blank lines before and after.
+# Your boolean tests should be simple. If they are complex, move their calculations
+# to variables earlier in your function, and use good names for the variable.
