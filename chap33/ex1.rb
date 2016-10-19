f79bd3ce93cf4fa5a3f0a007cd59c29796278ca8
Louisa <louisaspicer@gmail.com>
2016-10-19 14:51:17 +0100

    changed script to use a function

--- chap33/ex1.rb
+++ chap33/ex1.rb
@@ -1,15 +1,19 @@
-i = 0
-numbers = []
+def while_loop(top)
+  i = 0
+  numbers = []
 
-while i < 6
-  puts "At the top is i is #{i}"
-  numbers << i
+  while i < top
+    puts "At the top is i is #{i}"
+    numbers << i
 
-  i += 1
-  puts "Numbers now: ", numbers
-  puts "At the bottom i is #{i}"
+    i += 1
+    puts "Numbers now: ", numbers
+    puts "At the bottom i is #{i}"
+  end
+
+  puts "The numbers: "
+
+  numbers.each {|num| puts num }
 end
 
-puts "The numbers: "
-
-numbers.each {|num| puts num }
+while_loop(12)
