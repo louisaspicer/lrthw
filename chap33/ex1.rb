b76938da2f4fd683f389d98f7033bef453ea9a84
Louisa <louisaspicer@gmail.com>
2016-10-19 14:47:55 +0100

    while loop

---
+++ chap33/ex1.rb
@@ -0,0 +1,15 @@
+i = 0
+numbers = []
+
+while i < 6
+  puts "At the top is i is #{i}"
+  numbers << i
+
+  i += 1
+  puts "Numbers now: ", numbers
+  puts "At the bottom i is #{i}"
+end
+
+puts "The numbers: "
+
+numbers.each {|num| puts num }
