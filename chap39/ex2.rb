a7929faae8580ccd97cd40cffb24e1ef216a92cb
Louisa <louisaspicer@gmail.com>
2016-10-20 16:56:27 +0100

    playing around with some hash methods

--- chap39/ex2.rb
+++ chap39/ex2.rb
@@ -23,3 +23,12 @@ cities['HTF'] = 'Watford'
 puts cities
 
 puts "The city of #{counties['Kent']} is #{cities[counties['Kent']]}"
+
+cities.any? {|key, value| key == 'KT'}
+cities.each_key {|key| puts key}
+cities.flatten
+cities.has_key?('GT')
+
+GT = cities['GT']
+GT ||= 'No it does not exist'
+puts "Does GT exist? #{GT}"
