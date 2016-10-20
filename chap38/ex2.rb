604aa90802cb52b6582d25bca685c19db88a82ec
Louisa <louisaspicer@gmail.com>
2016-10-20 14:28:10 +0100

    playing around with array functions

---
+++ chap38/ex2.rb
@@ -0,0 +1,6 @@
+dvd_collection = ["sunshine of the spotless mind", "dude wheres my car", "something about mary"]
+
+with_index = dvd_collection.each_with_index {|dvd, index| puts "#{index}: #{dvd}"}
+
+puts dvd_collection.join(' ').split(' ').map(&:capitalize).join(' ')
+puts dvd_collection.join(" + ")
