0ecdc0e40182d9256fe8159ac30a16ccdfdf37e0
Louisa <louisaspicer@gmail.com>
2016-10-23 11:32:37 +0100

    playing with classes

---
+++ chap41/ex1.rb
@@ -0,0 +1,7 @@
+#Making notes on Learning to speak object-orientated:
+#Make a class named Student that is a Person
+#class Student has-a initialize that takes grade paraments
+#class Student has-a function named study that takes book parameters
+#set foo to an instance of class Student
+#from foo get the study function, and call it with a book parameter
+#from foo get the grade attribute and set it to A.
